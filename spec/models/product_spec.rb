7b02f54ddca087b01510ff7b88690aa3a0a229c4
Likai Liu <likailiu2@gmail.com>
2023-02-01 00:34:14 +0800

    Add 4 tests to test  the correct validation error messages are present

---
+++ spec/models/product_spec.rb
@@ -0,0 +1,26 @@
+require 'rails_helper'
+
+RSpec.describe Product, type: :model do
+  describe 'Validations' do
+    it 'error out when creating a new product without a name' do
+      @category = Category.create(name: 'evergreens')
+      @product = Product.create(name: nil, price: 998, quantity: 1, category: @category)
+      expect(@product.errors.full_messages).to include("Name can't be blank")
+    end
+    it 'error out when creating a new product without a price' do
+      @category = Category.create(name: 'evergreens')
+      @product = Product.create(name: 'some_plant', quantity: 1, category: @category)
+      pp @product
+      expect(@product.errors.full_messages).to include("Price can't be blank")
+    end
+    it 'error out when creating a new product without a quantity' do
+      @category = Category.create(name: 'evergreens')
+      @product = Product.create(name: 'some_plant', price: 998, quantity: nil, category: @category)
+      expect(@product.errors.full_messages).to include("Quantity can't be blank")
+    end
+    it 'error out when creating a new product without a category' do
+      @product = Product.create(name: 'some_plant', price: 998, quantity: 1, category: nil)
+      expect(@product.errors.full_messages).to include('Category must exist', "Category can't be blank")
+    end
+  end
+end
