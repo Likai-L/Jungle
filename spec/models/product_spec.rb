95bdcbabb4abfa6926288dc7a3ed22f02d3e52f2
Likai Liu <likailiu2@gmail.com>
2023-02-01 00:37:52 +0800

    Add a test where no validation error should be present

--- spec/models/product_spec.rb
+++ spec/models/product_spec.rb
@@ -2,6 +2,11 @@ require 'rails_helper'
 
 RSpec.describe Product, type: :model do
   describe 'Validations' do
+    it 'have no validation error when creating a new product without all fields filled' do
+      @category = Category.create(name: 'evergreens')
+      @product = Product.create(name: 'some_plant', price: 998, quantity: 1, category: @category)
+      expect(@product.errors.full_messages).to eq([])
+    end
     it 'have the right error message when creating a new product without a name' do
       @category = Category.create(name: 'evergreens')
       @product = Product.create(name: nil, price: 998, quantity: 1, category: @category)
@@ -10,7 +15,6 @@ RSpec.describe Product, type: :model do
     it 'have the right error message when creating a new product without a price' do
       @category = Category.create(name: 'evergreens')
       @product = Product.create(name: 'some_plant', quantity: 1, category: @category)
-      pp @product
       expect(@product.errors.full_messages).to include("Price can't be blank")
     end
     it 'have the right error message when creating a new product without a quantity' do
