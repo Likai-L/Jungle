aebb8bffc63d555d9635a68503c2c0760f26dd9e
Likai Liu <likailiu2@gmail.com>
2023-02-01 01:46:04 +0800

    Add validations and a authenticate method

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,3 +1,16 @@
 class User < ApplicationRecord
   has_secure_password
+  validates :password, presence: true
+  validates :password, confirmation: true
+  validates :password, length: { minimum: 3 }
+  validates :password_confirmation, presence: true
+  validates :email, presence: true
+  validates :email, uniqueness: true
+  validates :first_name, presence: true
+  validates :last_name, presence: true
+  def authenticate_with_credentials(email, password)
+    @user = User.where("email = #{email}").first
+    @user if @user[:password] == password
+    nil
+  end
 end
